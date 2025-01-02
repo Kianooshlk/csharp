a16c4f8c3d277918e5f2a248379cfcc646e6d25f
Kianooshlk <kialk2004@gmail.com>
2025-01-02 11:53:02 -0800

    Add files via upload

---
+++ c#/Login_open/Login_open/sigup.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Login_open
+{
+    partial class sigup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(sigup));
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.txtlast = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textlast = new System.Windows.Forms.Label();
+            this.textuser = new System.Windows.Forms.Label();
+            this.textpass = new System.Windows.Forms.Label();
+            this.textname = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(207, 280);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(366, 22);
+            this.txtpass.TabIndex = 0;
+            // 
+            // txtlast
+            // 
+            this.txtlast.Location = new System.Drawing.Point(389, 184);
+            this.txtlast.Name = "txtlast";
+            this.txtlast.Size = new System.Drawing.Size(184, 22);
+            this.txtlast.TabIndex = 1;
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(207, 184);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(166, 22);
+            this.txtname.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.button1.Location = new System.Drawing.Point(250, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(271, 57);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Sign up";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(207, 234);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(366, 22);
+            this.txtuser.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(207, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 7;
+            // 
+            // textlast
+            // 
+            this.textlast.AutoSize = true;
+            this.textlast.Location = new System.Drawing.Point(386, 162);
+            this.textlast.Name = "textlast";
+            this.textlast.Size = new System.Drawing.Size(72, 16);
+            this.textlast.TabIndex = 7;
+            this.textlast.Text = "Last Name";
+            // 
+            // textuser
+            // 
+            this.textuser.AutoSize = true;
+            this.textuser.Location = new System.Drawing.Point(207, 215);
+            this.textuser.Name = "textuser";
+            this.textuser.Size = new System.Drawing.Size(70, 16);
+            this.textuser.TabIndex = 7;
+            this.textuser.Text = "Username";
+            // 
+            // textpass
+            // 
+            this.textpass.AutoSize = true;
+            this.textpass.Location = new System.Drawing.Point(207, 259);
+            this.textpass.Name = "textpass";
+            this.textpass.Size = new System.Drawing.Size(67, 16);
+            this.textpass.TabIndex = 7;
+            this.textpass.Text = "Password";
+            // 
+            // textname
+            // 
+            this.textname.AutoSize = true;
+            this.textname.Location = new System.Drawing.Point(207, 162);
+            this.textname.Name = "textname";
+            this.textname.Size = new System.Drawing.Size(72, 16);
+            this.textname.TabIndex = 8;
+            this.textname.Text = "First Name";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Login_open.Properties.Resources.free_rockstar_logo_icon_download_in_svg_png_gif_file_formats__games_company_brand_world_logos_vol_9_pack_icons_282963;
+            this.pictureBox1.Location = new System.Drawing.Point(325, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(157, 112);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // sigup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Login_open.Properties.Resources.AAAAQQi0w_nNYvxL1q7tan_0bN3gZJNri6zP_1Sdx3dNmmguTecs94PDkDO1VW_uYK8M_dLH0wVt93BYZP8tSGKNxU20BPZiv2H4aTqQEBZE_MMjFQ56O1o1asNiy0fEozYT90gLd42BAkKTyDppqWBDH9SCq3Q;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(878, 438);
+            this.Controls.Add(this.textname);
+            this.Controls.Add(this.textpass);
+            this.Controls.Add(this.textuser);
+            this.Controls.Add(this.textlast);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.txtlast);
+            this.Controls.Add(this.txtpass);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "sigup";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sigup pag";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.TextBox txtlast;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label textlast;
+        private System.Windows.Forms.Label textuser;
+        private System.Windows.Forms.Label textpass;
+        private System.Windows.Forms.Label textname;
+    }
+}
